27f13fd0261203fcb7c2cafccb0485aa12ee6686
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-24 20:45:41 +0300

    layouts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,19 @@
 import '../global.css'
-import { Stack } from "expo-router";
+import { Slot } from 'expo-router'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ScrollView>
+      <SafeAreaView>
+        <View>
+          <Text className=" bg-red-500 p-8">My Expo App</Text>
+        </View>
+        <Slot />;
+        <View className="">
+          <Text className=" bg-blue-500 p-8 mb-0 ">© JJ</Text>
+        </View>
+      </SafeAreaView>
+    </ScrollView>
+  )
 }
