4cdbc21dadb0b56539e0f93f17937b02ee9f9412
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-24 23:58:03 +0300

    Grouping routes

---
+++ app/(auth)/forgot-password.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function FoegotPassword() {
+  return (
+    <View>
+      <Text>FoegotPassword</Text>
+    </View>
+  )
+}
