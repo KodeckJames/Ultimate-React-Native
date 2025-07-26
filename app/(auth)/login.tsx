9a2b74b09b296a724125376e6aacca3dfe3c30da
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-26 21:25:07 +0300

    Navigation

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import React from 'react'
+import { router } from 'expo-router'
 
 export default function LoginPage() {
   return (
     <View>
       <Text>LoginPage</Text>
+      <Button title="Login" onPress={() => router.push('/profile')} />
     </View>
   )
 }
+//In place of push, there are also replace which replaces the current route and you can't go back to it and back() for going back to the previous screen
--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,13 @@ import { View } from 'react-native'
 import { Link } from 'expo-router'
 
 export default function Index() {
+  //import Redirect from expo-router for this to work
+  // const isLoggedIn = true;
+  // if (!isLoggedIn) {
+  //   return <Redirect href={'/login'}/>
+  // }
   return (
+    
     <View className=" flex justify-center items-center min-h-screen">
       <Link href="/about">About Page</Link>
       <Link href="/profile">Profile Page</Link>
