e2f940c5e258935fac00d2c8d1f297203c222279
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-27 09:59:38 +0300

    Stack Navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,45 @@
 import '../global.css'
-import { Slot } from 'expo-router'
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { Stack } from 'expo-router'
+import { Pressable, Text, Image } from 'react-native'
+
+const LogoTitle = () => {
+  return (
+    <Image
+      width={30}
+      height={30}
+      source={{ uri: 'https://reactnative.dev/img/tiny_logo.png' }}
+    />
+  )
+}
 
 export default function RootLayout() {
   return (
-    <ScrollView>
-      <SafeAreaView>
-        <View>
-          <Text className=" bg-red-500 p-8">My Expo App</Text>
-        </View>
-        <Slot />;
-        <View className="">
-          <Text className=" bg-blue-500 p-8 mb-0 ">© JJ</Text>
-        </View>
-      </SafeAreaView>
-    </ScrollView>
+    <Stack
+      screenOptions={{
+        headerTintColor: 'yellow',
+        headerStyle: { backgroundColor: 'purple' },
+        headerShadowVisible: true,
+        headerTitleStyle: {
+          fontFamily: 'sans',
+          fontWeight: 'condensed',
+          color: 'yellow',
+        },
+        headerTitleAlign: 'center',
+        headerRight: () => (
+          <Pressable onPress={() => alert('Menu Button Pressed!')}>
+            <Text className=" text-yellow-500 font-bold">Menu</Text>
+          </Pressable>
+        ),
+        headerTitle: (props) => <LogoTitle />,
+      }}
+    >
+      <Stack.Screen name="index" />
+      <Stack.Screen
+        name="about/index"
+        options={{
+          title: 'About Page',
+        }}
+      />
+    </Stack>
   )
 }
