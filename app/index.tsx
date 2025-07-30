dd345353dccccf1f6b23fa692f0f7ce12bfac703
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-30 10:00:36 +0300

    Alerts

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,11 @@
-import { View, Pressable, Text, ActivityIndicator } from 'react-native'
+import {
+  View,
+  Pressable,
+  Text,
+  ActivityIndicator,
+  Button,
+  Alert,
+} from 'react-native'
 import { Link, router } from 'expo-router'
 
 export default function Index() {
@@ -8,19 +15,43 @@ export default function Index() {
   //   return <Redirect href={'/login'}/>
   // }
   return (
-    
     <View className=" flex justify-center items-center min-h-screen">
-      <ActivityIndicator size={'large'} color={'red'} animating={true } />
+      <ActivityIndicator size={'large'} color={'red'} animating={true} />
       <Link href="/about">About Page</Link>
       <Link href="/profile">Profile Page</Link>
       <Link href="/products">Products</Link>
       <Link href="/register">Register</Link>
       <Link href="/login">Login</Link>
       <Link href="/forgot-password">Forgot-password</Link>
-      <Pressable className=' bg-orange-500 p-2 rounded-lg' onPress={()=>router.push('/modal')}>
+      <Pressable
+        className=" bg-orange-500 p-2 rounded-lg"
+        onPress={() => router.push('/modal')}
+      >
         <Text>Modal</Text>
       </Pressable>
       <Link href={'/modal2'}>Modal2</Link>
+      <View>
+        <Button
+          title="Alert"
+          onPress={() =>
+            Alert.alert('Hello, World!', "How are Ya'll doing", [
+              {
+                text: 'Cancel',
+                onPress: () => console.log('Alert Canceled'),
+              },
+              {
+                text: 'Aloo',
+                onPress: () => console.log('Aloo Pressed'),
+              },
+              {
+                text: 'JJ',
+                onPress: () => console.log("It's me JJ"),
+              },
+            ])
+          }
+        />
+        <Button title='Alert2' onPress={()=>alert("Yoinks, this works too!")}/>
+      </View>
     </View>
   )
 }
