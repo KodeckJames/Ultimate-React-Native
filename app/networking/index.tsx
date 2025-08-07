c43e60a647549d942a1cbe6c4ca5b26ef8eeb36e
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-08-07 16:44:04 +0300

    Post Request

--- app/networking/index.tsx
+++ app/networking/index.tsx
@@ -6,6 +6,8 @@ import {
   FlatList,
   Text,
   ActivityIndicator,
+  TextInput,
+  Button,
 } from 'react-native'
 import { useState, useEffect } from 'react'
 
@@ -20,6 +22,9 @@ export default function Networking() {
   const [postList, setPostList] = useState<Post[]>([])
   const [isLoading, setIsLoading] = useState(true)
   const [refreshing, setRefreshing] = useState(false)
+  const [postTitle, setPostTitle] = useState('')
+  const [postBody, setPostBody] = useState('')
+  const [isPosting, setIsPosting] = useState(false)
 
   const fetchData = async (limit = 10) => {
     const response = await fetch(
@@ -33,7 +38,26 @@ export default function Networking() {
     setRefreshing(true)
     fetchData(20)
     setRefreshing(false)
-  }
+    }
+    
+    const addPost = async () => {
+        setIsPosting(true)
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+            method: "post",
+            headers: {
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify({
+                title: postTitle,
+                body: postBody
+            })
+        });
+        const newPost = await response.json();
+        setPostList([newPost, ...postList]);
+        setPostTitle("");
+        setPostBody("");
+        setIsPosting(false);
+    }
 
   useEffect(() => {
     fetchData()
@@ -48,38 +72,59 @@ export default function Networking() {
   }
   return (
     <SafeAreaView style={styles.container}>
-      <View style={styles.listContainer}>
-        <FlatList
-          data={postList}
-          renderItem={({ item }) => {
-            return (
-              <View style={styles.card}>
-                <Text style={styles.titleText}>{item.title}</Text>
-                <Text style={styles.bodyText}>{item.body}</Text>
-              </View>
-            )
-          }}
-          ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
-          ListHeaderComponent={
-            <Text
-              style={{
-                flex: 1,
-                justifyContent: 'center',
-                color: 'yellow',
-                fontSize: 26,
-                alignItems: 'center',
-              }}
-            >
-              Dummy Posts
-            </Text>
-          }
-          ListFooterComponent={
-            <Text style={{ color: 'red', fontSize: 20 }}>The End</Text>
-          }
-          refreshing={refreshing}
-          onRefresh={handleRefresh}
-        />
-      </View>
+      <>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Post Title"
+            value={postTitle}
+            onChangeText={setPostTitle}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Post Body"
+            value={postBody}
+            onChangeText={setPostBody}
+          />
+          <Button
+            title={isPosting ? 'Adding Post' : 'Add Post'}
+            onPress={addPost}
+            disabled={isPosting}
+          />
+        </View>
+        <View style={styles.listContainer}>
+          <FlatList
+            data={postList}
+            renderItem={({ item }) => {
+              return (
+                <View style={styles.card}>
+                  <Text style={styles.titleText}>{item.title}</Text>
+                  <Text style={styles.bodyText}>{item.body}</Text>
+                </View>
+              )
+            }}
+            ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+            ListHeaderComponent={
+              <Text
+                style={{
+                  flex: 1,
+                  justifyContent: 'center',
+                  color: 'yellow',
+                  fontSize: 26,
+                  alignItems: 'center',
+                }}
+              >
+                Dummy Posts
+              </Text>
+            }
+            ListFooterComponent={
+              <Text style={{ color: 'red', fontSize: 20 }}>The End</Text>
+            }
+            refreshing={refreshing}
+            onRefresh={handleRefresh}
+          />
+        </View>
+      </>
     </SafeAreaView>
   )
 }
@@ -113,5 +158,20 @@ const styles = StyleSheet.create({
     paddingTop: StatusBar.currentHeight,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  inputContainer: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+    margin: 16,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 8,
+    padding: 8,
+    borderRadius: 8,
   },
 })
