d14dea7d8f353153fe8f4c6deabc472a0fd06840
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-08-07 16:00:28 +0300

    Pull to refresh

--- app/networking/index.tsx
+++ app/networking/index.tsx
@@ -1,89 +1,117 @@
-import { StatusBar, StyleSheet, View, SafeAreaView, FlatList, Text,ActivityIndicator } from 'react-native'
-import {useState, useEffect} from 'react'
+import {
+  StatusBar,
+  StyleSheet,
+  View,
+  SafeAreaView,
+  FlatList,
+  Text,
+  ActivityIndicator,
+} from 'react-native'
+import { useState, useEffect } from 'react'
 
 type Post = {
-    userId: number;
-    id: number;
-    title: string;
-    body: string;
-};
+  userId: number
+  id: number
+  title: string
+  body: string
+}
 
 export default function Networking() {
+  const [postList, setPostList] = useState<Post[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const [refreshing, setRefreshing] = useState(false)
 
-    const [postList, setPostList] = useState<Post[]>([]);
-    const [isLoading, setIsLoading] = useState(true)
+  const fetchData = async (limit = 10) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+    )
+    const data = await response.json()
+    setPostList(data)
+    setIsLoading(false)
+  }
+  const handleRefresh = () => {
+    setRefreshing(true)
+    fetchData(20)
+    setRefreshing(false)
+  }
 
-    const fetchData = async (limit=10) => {
-        const response = await fetch(
-            `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
-        )
-        const data = await response.json();
-        setPostList(data);
-        setIsLoading(false)
-    }
+  useEffect(() => {
+    fetchData()
+  }, [])
 
-    useEffect(() => {
-        fetchData()
-    }, []);
-
-    if (isLoading) {
-        return (
-            <SafeAreaView style={styles.loadingContainer}>
-                <ActivityIndicator size="large" color={"blue"} />
-            </SafeAreaView>
-        )
-    }
+  if (isLoading) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color={'blue'} />
+      </SafeAreaView>
+    )
+  }
   return (
-      <SafeAreaView style={styles.container}>
-          <View style={styles.listContainer}>
-              <FlatList data={postList} renderItem={({item}) => {
-                  return (
-                      <View style={styles.card}>
-                          <Text style={styles.titleText}>{item.title}</Text>
-                          <Text style={styles.bodyText}>{item.body}</Text>
-                      </View>
-                  )
+    <SafeAreaView style={styles.container}>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={postList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.titleText}>{item.title}</Text>
+                <Text style={styles.bodyText}>{item.body}</Text>
+              </View>
+            )
+          }}
+          ItemSeparatorComponent={() => <View style={{ height: 20 }} />}
+          ListHeaderComponent={
+            <Text
+              style={{
+                flex: 1,
+                justifyContent: 'center',
+                color: 'yellow',
+                fontSize: 26,
+                alignItems: 'center',
               }}
-                  ItemSeparatorComponent={() => <View style={{ height: 20 }}
-                      
-                  />}   
-                  ListHeaderComponent={<Text style={{ flex: 1, justifyContent: "center", color: "yellow", fontSize: 26, alignItems: "center" }}>Dummy Posts</Text>}
-                  ListFooterComponent={<Text style={{color:"red", fontSize:20}}>The End</Text>}
-              />
+            >
+              Dummy Posts
+            </Text>
+          }
+          ListFooterComponent={
+            <Text style={{ color: 'red', fontSize: 20 }}>The End</Text>
+          }
+          refreshing={refreshing}
+          onRefresh={handleRefresh}
+        />
       </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "blue",
-        paddingTop: StatusBar.currentHeight
-    },
-    listContainer: {
-        flex: 1,
-        paddingHorizontal: 16
-    },
-    card: {
-        backgroundColor: "white",
-        padding: 16,
-        borderRadius: 8,
-        borderWidth:1
-    },
-    titleText: {
-        fontSize:30
-    },
-    bodyText: {
-        fontSize: 24,
-        color:"orange"
-    },
-    loadingContainer: {
-        flex: 1,
-        backgroundColor: "green",
-        paddingTop: StatusBar.currentHeight,
-        justifyContent: "center",
-        alignItems:"center"
-
-    }
+  container: {
+    flex: 1,
+    backgroundColor: 'blue',
+    paddingTop: StatusBar.currentHeight,
+  },
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16,
+  },
+  card: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  titleText: {
+    fontSize: 30,
+  },
+  bodyText: {
+    fontSize: 24,
+    color: 'orange',
+  },
+  loadingContainer: {
+    flex: 1,
+    backgroundColor: 'green',
+    paddingTop: StatusBar.currentHeight,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 })
